6f75b7b477ec5c642e6c54e3a733f9c1ce2c9b1e
BerkanN1 <berkannlbnt@hotmail.com>
2023-04-07 14:04:57 +0300

    getscore update

--- controllers/scoreController.js
+++ controllers/scoreController.js
@@ -13,3 +13,15 @@ export const createScore = async(req, res) =>{
      res.status(500).send("server error"+ err.message)
     }
  }
+ export const getScore = async (req, res) => {
+    try {
+      const { createdBy } = req.params;
+      const allNotes = await Score.find({ createdBy: createdBy });
+      const highestScore = allNotes.reduce((prev, current) => {
+        return (prev.score > current.score) ? prev : current;
+      });
+      res.status(200).send(highestScore);
+    } catch (error) {
+      res.status(500).send({ message: 'Server error' });
+    }
+  };
--- routes/scoreRoutes.js
+++ routes/scoreRoutes.js
@@ -1,11 +1,11 @@
 import Express from "express";
 
-import {createScore} from "../controllers/scoreController.js"
+import {createScore,getScore} from "../controllers/scoreController.js"
 
 const routes = Express.Router()
 
 
 routes.post('/create-score', createScore)
-
+routes.get('/get-score/:createdBy', getScore)
 
 export default routes
